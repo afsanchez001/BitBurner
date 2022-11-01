842a9eced13bfa75f17ebd35a5025a6ed18d9a67
Anthony F Sanchez <afsanchez001@hotmail.com>
2022-11-01 10:09:18 -0700

    Update bin.hk.js

--- bin.hk.js
+++ bin.hk.js
@@ -1,8 +1,8 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	let target = ns.args[0];
-	let repeat = ns.args[1];
-	while (repeat) {
+	let repeat = ns.args[1];	
+	do {
 		await ns.hack(target);
-	}
+	} while (repeat)
 }
